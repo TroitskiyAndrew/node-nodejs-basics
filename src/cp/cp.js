bcf6299e80186d3cdf8ec74de3c99a5f8e68fd05
Andrew <TroitskiyA@yandex.ru>
2022-06-02 21:15:05 +0500

    feat: complete cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,14 @@
+import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = spawn('node', [path.join(__dirname, 'files', 'script.js'), ...args]);
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
 };
+
+await spawnChildProcess(process.argv.slice(2))
